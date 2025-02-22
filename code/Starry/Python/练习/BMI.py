54a8f2b9161c958496bc3572fd4817c45d826a14
DLW_starry <127642222+starry777@users.noreply.github.com>
2025-02-22 13:40:43 +0800

    练习了python

---
+++ code/Starry/Python/练习/BMI.py
@@ -0,0 +1,16 @@
+def getcmd(wi, hi):
+    BMI = wi / hi**2
+    if BMI <= 18.5:
+        crd = "偏瘦"
+    elif BMI <= 24:
+        crd = "正常"
+    elif BMI <= 28:
+        crd = "偏胖"
+    else:
+        crd = "肥胖"
+    print(f"BMI指数为：{crd}{BMI}")
+
+
+h = float(input("请输入身高："))
+w = float(input("请输入体重："))
+getcmd(w, h)
